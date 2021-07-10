f5070b9fcbedbbea5da1c36cdd33bdd56c095a5e
ravi0818 <rkprasad0818@gmail.com>
2021-07-10 14:56:08 +0530

    fisrt commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", function (req, res) {
+  const age = Number(req.body.age);
+  res.send("Your age is " + age/15 + " Dog years");
+});
+app.listen(port);
